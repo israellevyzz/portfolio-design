58a2bbe297990750649067fcb6cbc175fa74237a
Israel Levy <israellima1808@gmail.com>
2025-10-15 14:29:55 -0300

    secund commit

--- script.js
+++ script.js
@@ -5,3 +5,13 @@ document.querySelectorAll('.navbar a').forEach(link => {
     alvo.scrollIntoView({ behavior: 'smooth' });
   });
 });
+
+//menu
+const menuToggle = document.querySelector('.menu-toggle');
+const nav = document.querySelector('.navbar nav');
+
+menuToggle.addEventListener('click', () => {
+  nav.classList.toggle('active');
+  menuToggle.classList.toggle('active');
+});
+
